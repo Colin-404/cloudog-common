0816ab8298b1662be871c2a99b053c726b0b6037
wenxi-3000 <1712472820@qq.com>
2025-04-06 16:52:29 +0800

    add JobResultRequest

--- job/requst_job.go
+++ job/requst_job.go
@@ -4,3 +4,9 @@ type JobCreateRequest struct {
 	JobType   string            `json:"job_type"`
 	JobParams map[string]string `json:"job_params"`
 }
+
+type JobResultRequest struct {
+	Time      string            `json:"time"`
+	JobType   string            `json:"job_type"`
+	JobParams map[string]string `json:"job_params"`
+}
