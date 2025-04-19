d86a7b48a29cc0a1469ec6d968023217667f3708
wenxi-3000 <1712472820@qq.com>
2025-04-19 18:41:22 +0800

    update job

--- comm/job.go
+++ comm/job.go
@@ -5,11 +5,7 @@ import "github.com/colin-404/cloudog-common/mproto"
 type Job struct {
 	ID       string            `json:"id"`
 	Status   string            `json:"status"`
+	Type     string            `json:"type"`
 	Params   map[string]string `json:"params"`
 	TaskList []mproto.Task     `json:"task_list"`
-}
-
-type JobRequest struct {
-	Type   string            `json:"type"`
-	Params map[string]string `json:"params"`
 }
