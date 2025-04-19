d462da5cb387e8090c017b87489eaad8f9b75f80
wenxi-3000 <1712472820@qq.com>
2025-04-19 13:28:55 +0800

    add job

--- comm/job.go
+++ comm/job.go
@@ -1,5 +1,14 @@
 package comm
 
+import "github.com/colin-404/cloudog-common/mproto"
+
+type Job struct {
+	ID       string            `json:"id"`
+	Status   string            `json:"status"`
+	Params   map[string]string `json:"params"`
+	TaskList []mproto.Task     `json:"task_list"`
+}
+
 type JobRequest struct {
 	Type   string            `json:"type"`
 	Params map[string]string `json:"params"`
