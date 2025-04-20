23d3b99ef5fc3d25968790b82e5abd1905e71863
wenxi-3000 <1712472820@qq.com>
2025-04-20 17:29:32 +0800

    update

--- comm/job.go
+++ comm/job.go
@@ -2,10 +2,21 @@ package comm
 
 import "github.com/colin-404/cloudog-common/mproto"
 
+const (
+	JobStatusPending  = "pending"
+	JobStatusRunning  = "running"
+	JobStatusSuccess  = "success"
+	JobStatusFailed   = "failed"
+	LinuxBaselineJob  = 500
+	AWSBaselineJob    = 501
+	AliyunBaselineJob = 502
+	KubeBaselineJob   = 503
+)
+
 type Job struct {
 	ID       string            `json:"id"`
 	Status   string            `json:"status"`
-	Type     string            `json:"type"`
+	Type     int32             `json:"type"`
 	Params   map[string]string `json:"params"`
 	TaskList []mproto.Task     `json:"task_list"`
 }
